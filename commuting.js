fb8b19084a0d3b3f8e4e4aa0a5ab00f4055a3a29
ian-r-rose <ian.r.rose@gmail.com>
2017-03-07 17:29:41 -0800

    Include direction information for locating stops.

--- commuting.js
+++ commuting.js
@@ -1,6 +1,7 @@
 const baseUrl = 'http://webservices.nextbus.com/service/publicJSONFeed';
 let agency = 'actransit';
-let route='6';
+let route='57';
+let direction='57_23_1';
 
 function assembleRequestUrl (args) {
   return baseUrl+'?'+args.join('&');
@@ -37,8 +38,10 @@ function getCurrentPosition() {
       resolve(position);
     }, (err)=>{
       resolve({
-        latitude: 37.8179231,
-        longitude: -122.2477552
+        coords: {
+          latitude: 37.8179231,
+          longitude: -122.2477552
+        }
       });
       //console.log('Cannot get position: ', err);
     });
@@ -53,12 +56,33 @@ function getNearestStop(agency, route, direction) {
   ]));
   locationRequest = getCurrentPosition();
   return Promise.all([nextBusRequest, locationRequest]).then( ([result, position])=>{
+    //Get the list of stop tags for the given
+    //direction, since they may not be the same
+    //both ways.
+    let stopsForDirection = undefined;
+    for(let dir of result.route.direction) {
+      if(dir.tag === direction) {
+        stopsForDirection = dir.stop;
+      }
+    }
+    if(!stopsForDirection) {
+      throw new Error('Cannot find the route direction');
+    }
+
+    //Only choose the stop if it is in the direction
+    //we are interested in
+    let stops = result.route.stop.filter((stop)=>{
+      return stopsForDirection.findIndex((el)=>{
+        return el.tag===stop.tag
+      }) !== -1});
+
+    //Find the stop that is closest to the user's position.
     let minDistance = 1.e99; //Should be big enough :)
     let nearestStop = result.route.stop[0];
-    for(let stop of result.route.stop) {
+    for(let stop of stops) {
       //Manhattan distance should be good enough for these purposes
-      let distance = Math.abs(stop.lat-position.latitude)+
-                     Math.abs(stop.lon-position.longitude);
+      let distance = Math.abs(stop.lat-position.coords.latitude)+
+                     Math.abs(stop.lon-position.coords.longitude);
       if(distance< minDistance) {
         minDistance = distance;
         nearestStop = stop;
@@ -77,7 +101,7 @@ function getPredictionForStop(route, stop) {
   ]));;
 }
 
-getNearestStop(agency, route).then((stop)=>{
+getNearestStop(agency, route, direction).then((stop)=>{
   getPredictionForStop(route, stop).then((prediction)=>{
     console.log(prediction);
   });
