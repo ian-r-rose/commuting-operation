f3b681c3015466a1de10e8d3c3589be852383d89
Ian Rose <ian.r.rose@gmail.com>
2017-03-17 17:51:01 -0700

    Restructure a bit.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { getPredictions, getDirectionForLine } from './commuting';
+import { Line, LineInfo, Prediction } from './components';
 
 let line = {
   agency: 'actransit',
@@ -34,56 +34,6 @@ class App extends Component {
         </div>
       </div>
     );
-  }
-}
-
-class Line extends Component {
-  render() {
-    return (
-      <div className="Line">
-        <LineInfo line={this.props.line} />
-        <Prediction line={this.props.line} />
-      </div>
-    );
-  }
-}
-
-class LineInfo extends Component {
-  render() {
-    let direction = getDirectionForLine(this.props.line);
-    return (
-      <div className="LineInfo">
-        <span>{line.displayId+' '}</span>
-        <span>{direction.displayId}</span>
-      </div>
-    );
-  }
-}
-
-class Prediction extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      prediction: [{
-        time: 'loading...',
-        isReliable: false,
-        isDelayed: false
-      }]
-    }
-  }
-
-  render() {
-    return (
-      <div className="Prediction">
-        <p>Prediction: {this.state.prediction[0].time}</p>
-      </div>
-    );
-  }
-
-  componentDidMount() {
-    getPredictions('actransit', '18').then((pred)=>{
-      this.setState({ prediction: pred });
-    });
   }
 }
 
