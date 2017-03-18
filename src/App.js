0cd0c6067c86081054e2523d6ab3b3c9c5683c8c
Ian Rose <ian.r.rose@gmail.com>
2017-03-17 17:45:53 -0700

    Start playing with React components.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { getPredictions } from './commuting';
+import { getPredictions, getDirectionForLine } from './commuting';
 
 let line = {
+  agency: 'actransit',
   id: '18',
   displayId: '18',
-  direction: '18_9_0',
-  displayDirection: ''
+  direction: [{
+    id: '18_9_0',
+    displayId: 'To Montclair'
+    }, {
+    id: '18_9_0',
+    displayId: 'To Montclair'
+  }],
+  changeoverTime: 12
 }
 
 class App extends Component {
@@ -20,11 +27,34 @@ class App extends Component {
           <h2>Commuting Operation</h2>
         </div>
         <div>
-              <Prediction line={line} />
-              <Prediction line={line} />
-              <Prediction line={line} />
-              <Prediction line={line} />
+          <Line line={line} />
+          <Line line={line} />
+          <Line line={line} />
+          <Line line={line} />
         </div>
+      </div>
+    );
+  }
+}
+
+class Line extends Component {
+  render() {
+    return (
+      <div className="Line">
+        <LineInfo line={this.props.line} />
+        <Prediction line={this.props.line} />
+      </div>
+    );
+  }
+}
+
+class LineInfo extends Component {
+  render() {
+    let direction = getDirectionForLine(this.props.line);
+    return (
+      <div className="LineInfo">
+        <span>{line.displayId+' '}</span>
+        <span>{direction.displayId}</span>
       </div>
     );
   }
@@ -45,7 +75,6 @@ class Prediction extends Component {
   render() {
     return (
       <div className="Prediction">
-        <p>Line: {this.props.line.displayId}</p>
         <p>Prediction: {this.state.prediction[0].time}</p>
       </div>
     );
